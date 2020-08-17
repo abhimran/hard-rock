c4f46ad54cd7b79c0684ac1b7e54aaa7af52c32d
Abdullah Imran <abhimrannn@gmail.com>
2020-08-17 23:37:00 +0600

    FInally Add Lyrics

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const api = {
   lyricsApi: "https://api.lyrics.ovh/v1/",
 };
 
-//Search Button 
+//Search Button
 const searchbox = document
   .getElementById("searchBtn")
   .addEventListener("click", function () {
@@ -12,7 +12,7 @@ const searchbox = document
     getResult(searchValue);
   });
 
-// Fetch Api
+// Fetch Api for songlist
 function getResult(songName) {
   fetch(`${api.songApi}${songName}`)
     .then((results) => results.json())
@@ -25,37 +25,45 @@ function DisplayResult(results) {
   resultCard.innerHTML = "";
 
   for (let i = 0; i < 10; i++) {
-
-    // fetch(
-    //     `${api.lyricsApi}${results.data[i].artist.name}/${results.data[i].title_short}`
-    //   )
-    //   .then((response) => response.json())
-    //   .then(Songlyrics=> console.log(Songlyrics))
-
     resultCard.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
                                     <div class="col-md-9">
                                         <div class="row">
                                             <div class="col-md-4">
-                                                <img src=${
-                                                  results.data[i].album.cover
-                                                } alt="not-found">
+                                                <img src=${results.data[i].album.cover} alt="not-found">
                                             </div>
                                             <div class="col-md-8">
-                                                <h3 class="lyrics-name">${
-                                                  results.data[i].title
-                                                }</h3>
-                                                <p class="author lead">Album by <span>${
-                                                  results.data[i].artist.name
-                                                }</span></p>
+                                                <h3 class="lyrics-name">${results.data[i].title}</h3>
+                                                <p class="author lead">Album by <span>${results.data[i].artist.name}</span></p>
                                             </div>
                                         </div>
                                     </div>
               
                                     <div class="col-md-3 text-md-right text-center">
-                                       <button onclick=""
-                                       class="btn btn-success"> Get Lyrics </button>
+                                       <button onclick="allLyrics('${results.data[i].artist.name}', '${results.data[i].title}')" class="btn btn-success"> GetLyrics
+                                       </button>
                                    </div>
 
                                   </div> `;
   }
+}
+
+// Fetch Api for All lyrics
+function allLyrics(name, title) {
+  fetch(`${api.lyricsApi}${name}/${title}`)
+    .then((response) => response.json())
+    .then(SingleSonglyrics);
+}
+
+// Dynamic single Lyrics
+function SingleSonglyrics(response) {
+  const resultCard = document.getElementById("resultCard");
+  resultCard.innerHTML = "";
+  resultCard.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
+                                <div class="row">
+                                            <div class="col-md-12">
+                                                <pre style="color: yellow">${response.lyrics ? response.lyrics :'Lyrics Not Found'}</pre>
+                                            </div>
+                                      </div>
+                            </div>
+                         `;
 }
